147ddadef4387337019d80fca47a4fe26416653a
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-07-09 17:04:21 -0700

    Add function_exists conditional in case ACF plugin isn't installed
    Previous ACF dependent get_field_objects function was triggering error on single painting pages with ACF plugin wasn't installed.

--- includes/section-paintings.php
+++ includes/section-paintings.php
@@ -3,21 +3,24 @@
     <?php the_content();?>
  
     <!-- Display Custom Fields -->
+    <?php if ( function_exists('get_field_objects')): ?>
+
     <?php $fields = get_field_objects();?>
-    <?php if( $fields ): ?>
+        <?php if( $fields ): ?>
 
-        <ul class="details list-group list-group-flush">
-            <?php foreach( $fields as $field ): ?>
-                
-                <?php if( $field['value'] ): ?>
-                    <li class="list-group-item">
-                        <strong><?php echo $field['label']; ?>:</strong> <?php echo $field['value']; ?>
-                    </li>
-                <?php endif; ?>
+            <ul class="details list-group list-group-flush">
+                <?php foreach( $fields as $field ): ?>
+                    
+                    <?php if( $field['value'] ): ?>
+                        <li class="list-group-item">
+                            <strong><?php echo $field['label']; ?>:</strong> <?php echo $field['value']; ?>
+                        </li>
+                    <?php endif; ?>
 
-            <?php endforeach; ?>
-        </ul>
+                <?php endforeach; ?>
+            </ul>
 
+        <?php endif; ?>
     <?php endif; ?>
 
 <?php endwhile; else: endif;?>
